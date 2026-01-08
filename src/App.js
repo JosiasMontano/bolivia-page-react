28514ce2d7bf78c50d63330d7d22d6d0fcdbb4f3
josias <josisamu10@gmail.com>
2026-01-08 12:34:45 -0400

    React Projet based on my old repository bolivia information page in jsx now in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,389 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const boliviaData = {
+    general: {
+        nombre: "Bolivia",
+        imagen: "https://www.educa.com.bo/sites/default/files/mapa%20politico%20de%20bolivia.jpg",
+        video: "https://www.youtube.com/embed/DnVOR09yp7o",
+        mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15340739.096698642!2d-67.63071895!3d-17.37519495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf8977bba295%3A0x1c9ec2bb0115ed67!2sBolivia!5e0!3m2!1ses!2sus!4v1620000000000!5m2!1ses!2sus",
+        extension: "1,098,581 km²",
+        poblacion: "12.3 millones (2023)",
+        altura: "90-6542 msnm",
+        densidad: "11 hab/km²",
+        capital: "Sucre (constitucional) y La Paz (sede de gobierno)",
+        idiomas: "Español, Quechua, Aymara y otras lenguas indígenas",
+        moneda: "Boliviano (BOB)",
+        gentilicio: "Boliviano/a",
+        economia: "Agricultura, minería, manufactura e hidrocarburos",
+        clima: "Varía desde tropical hasta polar",
+        festividades: "Carnaval de Oruro, Alasitas, Fiesta de la Virgen de Urkupiña",
+        gastronomia: "Salteñas, Pique Macho, Silpancho, Api con pastel",
+        idiomas_oficiales: "Español, Quechua, Aymara y 34 lenguas indígenas",
+        religion: "Predominantemente católica, protestante y religiones indígenas",
+        moneda: "Boliviano (BOB)",
+        sistema_gobierno: "República democrática, presidencialista y unitario",
+        principales_socios_comerciales: "Brasil, Argentina, China, Estados Unidos" 
+    },
+    departamentos: {
+        cochabamba: {
+            nombre: "Cochabamba",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-cochabamba-0116.jpg",
+            video: "https://www.youtube.com/embed/XjyawCcHhQc",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-66.5!3d-17.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Cochabamba!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "55,631 km²",
+            poblacion: "2,028,000 hab",
+            altura: "250-2,800 msnm",
+            densidad: "36 hab/km²",
+            idiomas: "Español, Quechua y Aymara (minoritario)",
+            gentilicio: "Cochabambino/a",
+            economia: "Agricultura, comercio y servicios, industria alimentaria y textiles",
+            clima: "Templado (18°C a 25°C promedio)",
+            festividades: "Fiesta de la Virgen de Urkupiña, Carnaval",
+            gastronomia: "Silpancho, Pique macho, Sopa de maní, Chicha cochabambina"
+        },
+        tarija: {
+            nombre: "Tarija",
+            imagen: "https://i.pinimg.com/originals/a8/ea/68/a8ea68616b039efd40d13459aaf1897b.jpg",
+            video: "https://www.youtube.com/embed/xyzeiUqUdqk",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-64.5!3d-21.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Tarija!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "37,623 km²",
+            poblacion: "600,000 ",
+            altura: "400-2,500 msnm",
+            densidad: "16 hab/km²",
+            idiomas: "Español, guaraní (Chaco)",
+            gentilicio: "Tarijeño/a",
+            economia: "Vitivinicultura, agricultura (maíz, papa), hidrocarburos",
+            clima: "Templado-cálido (15°C a 28°C), inviernos suaves",
+            festividades: "Fiesta de San Roque, Vendimia tarijeña",
+            gastronomia: "Saice, Ranga ranga, Chancho a la cruz, Vinos."
+        },
+        santacruz: {
+            nombre: "Santa Cruz",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-santa-cruz-0116.jpg",
+            video: "https://www.youtube.com/embed/V871IYGsa-s?si=5IZbZicl8hXQH4fY",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-62.5!3d-17.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93fbc3d6c1e2a0a5%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Santa%20Cruz!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "370,621 km²",
+            poblacion: "3,500,000 hab",
+            altura: "200-1500 msnm",
+        densidad: "9 hab/km²",
+        idiomas: "Español, guaraní, chiquitano y mojeño",
+        gentilicio: "Cruceño/a",
+        economia: "Agroindustria (soja, girasol), hidrocarburos, comercio, servicios",
+        clima: "Tropical (22°C a 35°C), con humedad alta en verano",
+        festividades: "Fiesta de la Virgen de Cotoca, Carnaval Cruceño",
+        gastronomia: "Majadito, Sonso, Locro, Empanadas de arroz."
+        },
+        lapaz: {
+            nombre: "La Paz",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-la-paz-0116.jpg",
+            video: "https://www.youtube.com/embed/AyOtlWnb0As?si=UNrThAPMdLsvKMhj",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-69.5!3d-16.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20La%20Paz!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "133,985 km²",
+            poblacion: "2,900,000 hab",
+            altura: "3,000 a 4,150 msnm",
+        densidad: "22 hab/km²",
+        idiomas: "Español, aymara (mayoritario), quechua",
+        gentilicio: "Paceño/a",
+        economia: "Servicios , Agricultura , Minería e Hidroeléctricas",
+        clima: "Frio (11°C a 25°C promedio)",
+        festividades: "Alasitas, Fiesta del Gran Poder",
+        gastronomia: "Plato Paceño, Chairo, Thimpu , Café de los Yungas."
+        },
+        oruro: {
+            nombre: "Oruro",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-oruro-0116.jpg",
+            video: "https://www.youtube.com/embed/0A3Aq-5hp8A?si=xAKrCj8LZpnHMjXG",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-68.5!3d-18.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Oruro!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "53,588 km²",
+            poblacion: "550,000 hab",
+            altura: "3,700-4,000 msnm",
+       densidad: "10 hab/km²",
+        idiomas: "Español, Quechua y Aymara",
+        gentilicio: "Oureño/a",
+        economia: "Minería (estaño, plata), comercio, turismo",
+        clima: "Frío y seco (5°C a 15°C)",
+        festividades: "Carnaval de Oruro, Fiesta del Socavón",
+        gastronomia: "Charquekán, Rostro asado, Api con pastel"
+        },
+        potosi: {
+            nombre: "Potosí",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-potosi-0116.jpg",
+            video: "https://www.youtube.com/embed/VbRV4WNBzKI?si=ucExX-8kDjsTwULr",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-67.5!3d-20.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Potos%C3%AD!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "118,218 km²",
+            poblacion: "900,000 hab",
+            altura: "4,000 msnm (promedio)",
+        densidad: "7 hab/km²",
+        idiomas: "Español, Quechua",
+        gentilicio: "Potosino/a",
+        economia: "Minería (plata, zinc, plomo), turismo",
+        clima: "Frío y seco (0°C a 15°C)",
+        festividades: "Fiesta de Ch'utillos, Carnaval Minero",
+        gastronomia: "K'ala phurka, Ají de arvejas, Asado potosino"
+        },
+        pando: {
+            nombre: "Pando",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-pando-0116.jpg",
+            video: "https://www.youtube.com/embed/CyP8TRkOPZQ?si=q4NARG1st4ZikCc9",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-67.5!3d-11.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Pando!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "63,827 km²",
+            poblacion: "154,000 hab",
+            altura: "180 msnm (promedio)",
+        densidad: "2 hab/km²",
+        idiomas: "Español, portugués (regional)",
+        gentilicio: "Pandino/a",
+        economia: "Castaña, madera, turismo ecológico",
+        clima: "Tropical húmedo (25°C a 35°C)",
+        festividades: "Feria de la Castaña, Aniversario de Pando",
+        gastronomia: "Majao, Pacumutu, Asado de jochi"
+        },
+        beni: {
+            nombre: "Beni",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-beni-0116.jpg",
+            video: "https://www.youtube.com/embed/vu9Xz9HSboo?si=0UyVZD6lJHFPZJpO",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-65.5!3d-14.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20del%20Beni!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "213,564 km²",
+            poblacion: "480,000 hab",
+            altura: "150 msnm",
+        densidad: "2 hab/km²",
+        idiomas: "Español, mojeño, trinitario (lenguas indígenas)",
+        gentilicio: "Beniano/a",
+        economia: "Ganadería, agricultura (arroz, maíz), turismo ecológico",
+        clima: "Tropical húmedo (23°C a 35°C)",
+        festividades: "Fiesta del Santo Patrono de Trinidad, Feria Exposición",
+        gastronomia: "Masaco, Keperi, Majao, Sopa de maní"
+        },
+        sucre: {
+            nombre: "Sucre",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-chuquisaca-0116.jpg",
+            video: "https://www.youtube.com/embed/PP-mRDeQCrs?si=JF76ZkvOZsFmZreF",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-65.5!3d-20.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Chuquisaca!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "51,524 km²",
+            poblacion: "637,000 hab",
+            altura: "50-3500 msnm",
+        densidad: "12 hab/km²",
+        idiomas: "Español, quechua, guaraní (Chaco)",
+        gentilicio: "Chuquisaqueño/a",
+        economia: "Agricultura (maíz, trigo), turismo histórico, servicios educativos",
+        clima: "Variado (templado en valles, cálido en el Chaco)",
+        festividades: "Pujllay, Festival de la Cultura",
+        gastronomia: "Chorizos chuquisaqueños, Sopa de maní, Ají de papalisa"
+        }
+    }
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentView, setCurrentView] = useState('bolivia');
+    
+    const renderMainPage = () => (
+        <div>
+            <h2>Bolivia: Información General</h2>
+            <img 
+                src={boliviaData.general.imagen} 
+                alt="Mapa de Bolivia" 
+                className="main-image"
+            />
+            
+            <div className="media-container">
+                <div className="map-container">
+                    <iframe 
+                        src={boliviaData.general.mapa}
+                        title="Mapa de Bolivia"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+                <div className="video-container">
+                    <iframe 
+                        src={boliviaData.general.video}
+                        title="Video de Bolivia"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+            </div>
+            
+            <div className="btn-container">
+                {Object.keys(boliviaData.departamentos).map(depto => (
+                    <button 
+                        key={depto}
+                        className="btn"
+                        onClick={() => setCurrentView(depto)}
+                    >
+                        {boliviaData.departamentos[depto].nombre}
+                    </button>
+                ))}
+            </div>
+
+            <div className="info-section">
+                <div className="info-grid">
+                    <div className="info-item">
+                        <h3> 🌐 Extensión Territorial</h3>
+                        <p>{boliviaData.general.extension}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 👥 Población</h3>
+                        <p>{boliviaData.general.poblacion}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> ⛰️ Altura</h3>
+                        <p>{boliviaData.general.altura}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 📊 Densidad</h3>
+                        <p>{boliviaData.general.densidad}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 🏛️ Capital</h3>
+                        <p>{boliviaData.general.capital}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 🗣️ Idiomas</h3>
+                        <p>{boliviaData.general.idiomas}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 💰 Moneda</h3>
+                        <p>{boliviaData.general.moneda}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 🤝 Gentilicio</h3>
+                        <p>{boliviaData.general.gentilicio}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 🏭 Economia</h3>
+                        <p>{boliviaData.general.economia}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> ☀️ Clima</h3>
+                        <p>{boliviaData.general.clima}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 🎉 Festividades</h3>
+                        <p>{boliviaData.general.festividades}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 📜 Idiomas Oficiales</h3>
+                        <p>{boliviaData.general.idiomas_oficiales}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> ⛪ Religion</h3>
+                        <p>{boliviaData.general.religion}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 💵 Moneda</h3>
+                        <p>{boliviaData.general.moneda}</p>
+                    </div>
+                    <div className="info-item">
+                        <h3> 🏛️ Sistema Gobierno</h3>
+                        <p>{boliviaData.general.sistema_gobierno}</p>
+                    </div><div className="info-item">
+                        <h3> 🤝 Principales Socios Comerciales</h3>
+                        <p>{boliviaData.general.principales_socios_comerciales}</p>
+                    </div>
+                </div>
+            </div>
+            
+            
+        </div>
+    );
+    
+    const renderDepartmentPage = (deptoKey) => {
+        const depto = boliviaData.departamentos[deptoKey];
+        
+        return (
+            <div>
+                <button 
+                    className="btn back-btn"
+                    onClick={() => setCurrentView('bolivia')}
+                >
+                    ← Volver a Bolivia
+                </button>
+                
+                <h2>{depto.nombre}</h2>
+                <img 
+                    src={depto.imagen} 
+                    alt={`Mapa de ${depto.nombre}`} 
+                    className="main-image"
+                />
+                
+                <div className="media-container">
+                    <div className="map-container">
+                        <iframe 
+                            src={depto.mapa}
+                            title={`Mapa de ${depto.nombre}`}
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                    <div className="video-container">
+                        <iframe 
+                            src={depto.video}
+                            title={`Video de ${depto.nombre}`}
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                </div>
+
+                <div className="info-section">
+                    <div className="info-grid">
+                        <div className="info-item">
+                            <h3>Extensión Territorial</h3>
+                            <p>{depto.extension}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Población</h3>
+                            <p>{depto.poblacion}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Altura</h3>
+                            <p>{depto.altura}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Densidad</h3>
+                            <p>{depto.densidad}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Idiomas</h3>
+                            <p>{depto.idiomas}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Gentilicio</h3>
+                            <p>{depto.gentilicio}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Economia</h3>
+                            <p>{depto.economia}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Clima</h3>
+                            <p>{depto.clima}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Festividades</h3>
+                            <p>{depto.festividades}</p>
+                        </div>
+                        <div className="info-item">
+                            <h3>Gastronomia</h3>
+                            <p>{depto.gastronomia}</p>
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
+        );
+    };
+    
+    return (
+        <div className="container">
+            <header className="header">
+                <h1>Bolivia y sus Departamentos</h1>
+            </header>
+            
+            <main>
+                {currentView === 'bolivia' ? renderMainPage() : renderDepartmentPage(currentView)}
+            </main>
+            <footer>.<p>Derechos reservados 2025</p></footer>
+        </div>
+    );
 }
 
 export default App;
